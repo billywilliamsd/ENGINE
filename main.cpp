082b56716628e0e81e3b5bbc6f89e0ede8b28761
rebukeiscool <leehbryson@gmail.com>
2025-08-10 19:08:49 -0500

    engine and texturemanager abstraction

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+#include "engine.h"
 #include <iostream>
+using namespace std;
 
 int main(int argc, char** argv){
+    cout << "Hello World!\n";
+
+    Engine::GetInstance()->Init();
+
+    while(Engine::GetInstance()->IsRunning()){
+        Engine::GetInstance()->Events();
+        Engine::GetInstance()->Update();
+        Engine::GetInstance()->Render();
+    }
+
+    Engine::GetInstance()->Clean();
     return 0;
 }
